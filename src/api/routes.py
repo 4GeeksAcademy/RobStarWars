a0099556306fc178dfd5f9d52405e6e139814fc3
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-05-29 17:32:50 +0000

    nuevo

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, User
+from api.models import db, Users
 
 
 api = Blueprint('api', __name__)
@@ -13,6 +13,6 @@ CORS(api)  # Allow CORS requests to this API
 
 @api.route('/hello', methods=['GET'])
 def handle_hello():
-    response_body = []
+    response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend"
     return response_body, 200
