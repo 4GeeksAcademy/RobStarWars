2f44f20106c435328bab31fc5da93aa3640a1540
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-07-26 10:20:08 +0200

    Update Characters.jsx

--- src/front/js/pages/Characters.jsx
+++ src/front/js/pages/Characters.jsx
@@ -5,36 +5,7 @@ import { Context } from "../store/appContext";
 export const Characters = () => {
     const { store, actions } = useContext(Context); // Asegúrate de obtener actions aquí
     
-    const handleAddFavorite = async (characterId) => {
-        try {
-            const userId = store.currentUser;  
-            
-            // Verifica que userId esté definido
-            if (!userId) {
-                alert("No user is logged in.");
-                return;
-            }
 
-            const response = await fetch(`${process.env.BACKEND_URL}/api/favorites`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ favorited_by_id: userId, character_id: characterId })
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            
-            await actions.updateFavorites(); // Llama a la función para actualizar favoritos
-            
-            alert("Favorite added successfully!");
-        } catch (error) {
-            console.error('Error adding favorite:', error);
-            alert("Failed to add favorite.");
-        }
-    };
 
     return (
         <div>
@@ -51,7 +22,7 @@ export const Characters = () => {
                             <div className="d-flex justify-content-between align-items-center">
                                 <Link to={'/character-details/' + index} onClick={() => actions.setCurrentItem(item)} className="btn btn-primary mt-2">Más Info</Link>
                                 <i className="fa-solid fa-heart btn btn-danger" style={{width: "40px", height: "30px"}}
-                                   onClick={() => store.addFavorite}></i>
+                                   onClick={() => store.addFavorite(item.name)}></i>
                             </div>
                         </div>
                     </div>
